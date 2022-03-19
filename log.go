2ebfc6a0a166d87f4a3fd7e4e84af158081993d3
wenjiangyi <504038824@qq.com>
2022-03-19 09:21:40 +0800

    NewLogger 方法返回Logger接口

--- log.go
+++ log.go
@@ -6,11 +6,7 @@ import (
 	"github.com/natefinch/lumberjack"
 )
 
-type Log struct {
-	logger Logger
-}
-
-func NewLogger(filename string, maxSize, maxDay int, driver, level string, debug bool) *Log {
+func NewLogger(filename string, maxSize, maxDay int, driver, level string, debug bool) Logger {
 	hook := lumberjack.Logger{
 		Filename:   filename, // 日志文件路径
 		MaxSize:    maxSize,  // 每个日志文件保存的大小 单位:M
@@ -20,36 +16,16 @@ func NewLogger(filename string, maxSize, maxDay int, driver, level string, debug
 		LocalTime:  true,
 	}
 
-	logger := &Log{}
+	var logger Logger
 	switch driver {
 	case "zap":
 		if level, ok := LevelMap[level]; !ok {
 			panic(fmt.Errorf("Unknown log level.\n"))
 		} else {
-			logger.logger = RegisterZapLogger(level, &hook, debug)
+			logger = RegisterZapLogger(level, &hook, debug)
 		}
 	default:
 		panic(fmt.Errorf("Unknown log driver.\n"))
 	}
 	return logger
-}
-
-func (l *Log) Debugf(msg string, args ...Extra) {
-	l.logger.Debugf(msg, args)
-}
-
-func (l *Log) Infof(msg string, args ...Extra) {
-	l.logger.Infof(msg, args)
-}
-
-func (l *Log) Warnf(msg string, args ...Extra) {
-	l.logger.Warnf(msg, args)
-}
-
-func (l *Log) Errorf(msg string, args ...Extra) {
-	l.logger.Errorf(msg, args)
-}
-
-func (l *Log) Fatalf(msg string, args ...Extra) {
-	l.logger.Fatalf(msg, args)
 }
